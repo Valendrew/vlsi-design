d3a301a6aa20b6b7780b8250647a00709b831e07
DanieleMorotti <daniele.morotti.99@gmail.com>
2022-08-27 14:27:59 +0200

    feat: working smt base implementation and z3 solver test

--- SMT/src/build_models.py
+++ SMT/src/build_models.py
@@ -1,11 +1,13 @@
 from os.path import join as join_path
-import math
+import math, time
 import z3
 
 import sys
 sys.path.append("./")
 
-from utils.smt_utils import extract_input_from_txt
+from utils.smt_utils import extract_input_from_txt, z3_parse_solution, get_w_and_h_from_txt
+from utils.types import SolverSMT, LogicSMT, ModelEnum
+from utils.plot import plot_cmap
 
 root_path = "./SMT"
 plot_path = join_path(root_path, "out/plots/{model}/{file}")
@@ -17,19 +19,23 @@ data_path = {
 
 
 # We append to the string all the script to avoid writing it manually
-def build_model(W, N, widths, heights):
+def build_model(W, N, widths, heights, logic: LogicSMT="LIA"):
     # Lower and upper bounds for the height
     l_low = math.ceil(sum([widths[i]*heights[i] for i in range(N)]) / W)
     l_up = sum(heights)
     lines = []
 
-    lines.append("(declare-fun coord_x (Int) Int)")
-    lines.append("(declare-fun coord_y (Int) Int)")
-    #lines.append("(declare-fun l () Int)")
+    # Options
+    lines.append(f"(set-logic {logic})")
+
+    # Variables declaration
+    lines += [f"(declare-fun coord_x{i} () Int)" for i in range(N)]
+    lines += [f"(declare-fun coord_y{i} () Int)" for i in range(N)]
+    lines.append("(declare-fun l () Int)")
 
     # Domain of variables
-    lines += [f"(assert (and (>= (select coord_x {i}) 0) (<= (select coord_x {i}) {W-min(widths)})))" for i in range(N)]
-    lines += [f"(assert (and (>= (select coord_y {i}) 0) (<= (select coord_y {i}) {l_up-min(heights)})))" for i in range(N)]
+    lines += [f"(assert (and (>= coord_x{i} 0) (<= coord_x{i} {W-min(widths)})))" for i in range(N)]
+    lines += [f"(assert (and (>= coord_y{i} 0) (<= coord_y{i} {l_up-min(heights)})))" for i in range(N)]
     lines.append(f"(assert (and (>= l {l_low}) (<= l {l_up})))")
 
 
@@ -37,37 +43,81 @@ def build_model(W, N, widths, heights):
     for i in range(N):
         for j in range(N):
             if i < j:
-                lines.append(f"(assert (or (<= (+ (select coord_x {i}) {widths[i]}) (select coord_x {j})) "
-                                         f"(<= (+ (select coord_y {i}) {heights[i]}) (select coord_y {j})) "
-                                         f"(>= (- (select coord_x {i}) {widths[j]}) (select coord_x {j})) "
-                                         f"(>= (- (select coord_y {i}) {heights[i]}) (select coord_y {j}))))"
+                lines.append(f"(assert (or (<= (+ coord_x{i} {widths[i]}) coord_x{j}) "
+                                         f"(<= (+ coord_y{i} {heights[i]}) coord_y{j}) "
+                                         f"(>= (- coord_x{i} {widths[j]}) coord_x{j}) "
+                                         f"(>= (- coord_y{i} {heights[j]}) coord_y{j})))"
                             )
 
 
     # Boundary constraints
-    lines += [f"(assert (and (<= (+ (select coord_x {i}) {widths[i]}) {W}) (<= (+ (select coord_y {i}) {heights[i]}) l)))" for i in range(N)]
+    lines += [f"(assert (and (<= (+ coord_x{i} {widths[i]}) {W}) (<= (+ coord_y{i} {heights[i]}) l)))" for i in range(N)]
 
 
     lines.append("(check-sat)")
-    #lines.append(f"(get-value ({' '.join([f'(select pos {i})' for i in range(N)])}))")
+    lines.append("(get-model)")
     
     with open(f"{root_path}/src/model.smt2", "w+") as f:
         for line in lines:
             f.write(line + '\n')
-    
 
 
-def run_solver():
-    solver = z3.Optimize()
-    l = z3.Int("l")
-    solver.add(l)
+def run_solver(heights, timeout=300):
+    solver = z3.Solver()
+    solver.set(timeout=timeout*1000)
     smt_mod = z3.parse_smt2_file(f"{root_path}/src/model.smt2")
     solver.add(smt_mod)
-    solver.minimize(l)
+
+    l_up = sum(heights)
+    solution = {"l": l_up, "coord_x":[], "coord_y":[]}
+    start_time = time.perf_counter()
+    while True:
+        res = solver.check()
+        # I need to manage the timeout decreasing during the solve
+        check_time = time.perf_counter()
+        new_timeout = int(timeout*1000-(check_time-start_time)*1000)
+        if new_timeout < 0:
+            print("Timeout reached, search stopped.")
+            break
+        solver.set("timeout", new_timeout)
+
+        if res == z3.unsat:
+            print("Unsat therefore search interrupted.")
+            break
+        if res == z3.unknown:
+            if solver.reason_unknown() == "timeout":
+                print("Timeout reached, search stopped.")
+            else:
+                print("Error during the search, unknown status returned.")
+            break
+        last_model = solver.model()
+        
+        l_ind = [str(m) for m in last_model].index('l')
+        l_var = last_model[l_ind]
+        
+        solver.add(l_var() != last_model[l_var()])
+        l, coord_x, coord_y = z3_parse_solution(last_model)
+        if l < solution["l"]:
+            solution = {"l": l, "coord_x": coord_x, "coord_y": coord_y}
     
+    end_time = time.perf_counter()
+    if solution["l"] != l_up:
+        print(f"The minimum found l is {solution['l']}, in {end_time - start_time} seconds.")
+        return solution["l"], solution["coord_x"], solution["coord_y"]
+    else:
+        print("No solutions found within the time limit")
+        return None, None, None
 
 if __name__ == "__main__":
-    W, N, widths, heights = extract_input_from_txt(data_path["txt"], "ins-10.txt")
-    build_model(W, N, widths, heights)
-    solver = z3.Optimize()
-    run_solver()
+
+    instance_file = "ins-11.txt"
+    plot_file = plot_path.format(model=ModelEnum.BASE.value, file=instance_file.split(".")[0])
+
+    W, N, widths, heights = extract_input_from_txt(data_path["txt"], instance_file)
+    build_model(W, N, widths, heights, logic=LogicSMT.LIA.value)
+    l, coord_x, coord_y = run_solver(heights)
+    circuits = get_w_and_h_from_txt(instance_file)
+    if l is not None:
+        plot_cmap(
+                W, l, N, circuits, {"x": coord_x, "y": coord_y}, plot_file, cmap_name="turbo_r"
+            )
