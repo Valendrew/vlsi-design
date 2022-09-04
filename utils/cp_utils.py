a4cabec5bc161c290e77feb2938b35f8dfefef65
DanieleMorotti <daniele.morotti.99@gmail.com>
2022-09-04 10:21:57 +0200

    refact: some changes in minizinc and smt files

--- utils/cp_utils.py
+++ utils/cp_utils.py
@@ -1,21 +1,21 @@
 import argparse
-
 from utils.types import DEFAULT_TIMEOUT, ModelType, SolverMinizinc
 
 
 def parse_cp_argument():
     parser = argparse.ArgumentParser(description="CP solver for VLSI")
-    parser.add_argument("instance")
+    mode = parser.add_mutually_exclusive_group(required=True)
+    mode.add_argument("-ins", "--instance", help="The name of the instance file without extension (i.e. ins-1 and not ins-1.dzn)")
     parser.add_argument("-m", "--model", default=ModelType.BASE, choices=[e.value for e in ModelType])
     parser.add_argument("-s", "--solver", default=SolverMinizinc.GECODE, choices=[e.value for e in SolverMinizinc])
     parser.add_argument("-f", "--freesearch", action="store_true", default=False)
     parser.add_argument("-t", "--timeout", default=DEFAULT_TIMEOUT, type=int)
     parser.add_argument("-v", "--verbose", action="store_true", default=False)
-    # TODO merge statistics with instance using append or something similiar
-    parser.add_argument("-st", "--statistics", action="store_true", default=False)
+    mode.add_argument("-test", "--testing", type=int, nargs=2, help="Specify the interval of instances to run the solver on.")
 
     args = parser.parse_args()
     return vars(args)
+
 
 def check_cp_solver_exists(solver: SolverMinizinc):
     if solver == SolverMinizinc.CHUFFED:
